dfcd72038010acc5f23404d13bb6f95ab2ad0436
Rihan Mohammed <rihanmohammed26@gmail.com>
2022-07-08 14:35:02 +0530

    search bar complete

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+const SearchScreen = () => {
+	const [term, setTerm] = useState('');
+
+	return (
+		<View style={styles.backGround}>
+			<SearchBar
+				term={term}
+				onTermChange={newTerm => setTerm(newTerm)}
+				onTermSubmit={() => console.log('term was submitted')}
+			/>
+			<Text>Search Screen</Text>
+			<Text>{term}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	backGround: {
+		backgroundColor: "#fff"
+	}
+});
+
+export default SearchScreen;
